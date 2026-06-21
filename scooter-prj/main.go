0c2a54e7798a232598d4ccb8bf4adb59b5d6da70
Michał <michal.lejwoda@gmail.com>
2026-06-21 19:47:04 +0200

    Connect database

--- scooter-prj/main.go
+++ scooter-prj/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	urlExample := "postgres://Test:password@db:5432/test"
 	fmt.Println("Test")
+	dbpool, err := pgxpool.New(context.Background(), urlExample)
+	if err != nil {
+		log.Fatalf("Cant connect to database")
+	}
+	defer dbpool.Close()
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+	}
+	fmt.Println(greeting)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Go lang server works")
 	})
+
 	fmt.Println("test")
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
