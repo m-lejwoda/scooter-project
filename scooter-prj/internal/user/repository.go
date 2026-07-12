2f454fb027921b1f3a357109119d12e0fc7f4cf3
Michał <michal.lejwoda@gmail.com>
2026-07-12 12:18:08 +0200

    fix function name

--- scooter-prj/internal/user/repository.go
+++ scooter-prj/internal/user/repository.go
@@ -6,6 +6,6 @@ import (
 )
 
 type UserRepository interface {
-	getByID(ctx context.Context, id string) (*User, error)
+	GetByID(ctx context.Context, id string) (*User, error)
 	Save(ctx context.Context, user *User) error
 }
