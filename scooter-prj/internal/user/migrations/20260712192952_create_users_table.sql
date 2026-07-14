61510a18efa5d1d4a306b41e68395aaf8474d8f6
Michał <michal.lejwoda@gmail.com>
2026-07-14 20:19:18 +0200

    feature goose working with postgres

--- scooter-prj/internal/user/migrations/20260712192952_create_users_table.sql
+++ scooter-prj/internal/user/migrations/20260712192952_create_users_table.sql
@@ -4,8 +4,8 @@ CREATE TABLE users (
   name VARCHAR(100) NOT NULL CHECK (length(trim(name)) >= 2),
   email VARCHAR(255) NOT NULL UNIQUE CHECK (length(trim(email)) >= 5),
   last_name VARCHAR(100),
-  password VARCHAR(255) NOT NULL 
+  password VARCHAR(255) NOT NULL, 
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
 -- +goose Down
 DROP TABLE users;
